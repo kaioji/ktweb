ac00bc0b955f0a7069c76bd4d112a49e12b380f9
kaioji <kaioji@tencent.com>
2024-10-26 17:57:38 +0800

    feat: 使用1.8开发

--- src/main/kotlin/com/example/demo/book/model/BookCreateRequest.kt
+++ src/main/kotlin/com/example/demo/book/model/BookCreateRequest.kt
@@ -1,14 +1,9 @@
 package com.example.demo.book.model
 
 import java.io.Serializable
-import javax.validation.constraints.NotBlank
 
 data class BookCreateRequest(
-    @NotBlank
     var title: String,
-
-    @NotBlank
     var author: String,
-
     var name: String
 ) : Serializable
