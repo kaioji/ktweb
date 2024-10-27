0064c223d4bac93625ad23d45e317c72b813170d
kaioji <kaioji@tencent.com>
2024-10-27 16:39:30 +0800

    feat: 创建时保证幂等性

--- src/main/kotlin/com/example/demo/vip/service/VipService.kt
+++ src/main/kotlin/com/example/demo/vip/service/VipService.kt
@@ -7,11 +7,20 @@ import com.example.demo.vip.model.CreateCardResponse
 import com.example.demo.vip.model.QueryCardRequest
 import com.google.gson.Gson
 import jakarta.annotation.Resource
+import org.hibernate.exception.ConstraintViolationException
 import org.modelmapper.ModelMapper
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+
+/*
+* 幂等性实现方案：
+* 1.数据库唯一索引;2.乐观锁；3.分布式锁；4.防重表；5.token令牌；6.状态机控制；7.来源+序列号。
+* */
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED)
 class VipService(
     private val vipCardRepository: VipCardRepository
 ) {
@@ -25,13 +34,18 @@ class VipService(
 
     fun createVip(request: CreateCardRequest): CreateCardResponse {
         log.info("createCard request: ${gson.toJson(request)}")
-        val hasVip = vipCardRepository.findByUin(request.uin)
+
         //  通过唯一键保证幂等性
-        if (hasVip != null) {
-            return CreateCardResponse()
+        var entity: VipCardEntity
+        try {
+            entity = modelMapper.map(request, VipCardEntity::class.java)
+            vipCardRepository.saveAndFlush(entity)
+        } catch (e: ConstraintViolationException) {
+            entity = vipCardRepository.findByUin(request.uin)!!
+        } catch (e: Exception) {
+            log.error(e.message)
+            throw e
         }
-        val entity = modelMapper.map(request, VipCardEntity::class.java)
-        vipCardRepository.saveAndFlush(entity)
         return modelMapper.map(entity, CreateCardResponse::class.java)
     }
 
@@ -40,3 +54,5 @@ class VipService(
         return CreateCardResponse()
     }
 }
+
+
